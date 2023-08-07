4eec8674483e47096f22b1f71a0311b54493dda0
wiljo7 <wiljo7hdz@gmail.com>
2023-08-07 15:32:19 -0400

    'automate'

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import pandas as pd
 import datetime
 
 
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 c=open('configure.txt','r')
 lc=c.readlines() 
 info_dict={}
